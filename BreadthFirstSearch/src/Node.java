0e9059aeaf1d853a29b527a342af0fe72ed44c54
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2018-06-08 09:29:25 -0400

    aDGJionsag

--- BreadthFirstSearch/src/Node.java
+++ BreadthFirstSearch/src/Node.java
@@ -1,4 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Node {
+	
+	public String stationName;
+	Node leftChild;
+	Node rightchild;
+	Node middleChild;
+	
+	public Node(String stationName, Node firstChild, Node secondChild, Node thirdChild)
+	{
+		this.stationName = stationName;
+		this.leftChild = firstChild;
+		this.middleChild = secondChild;
+		this.rightchild = thirdChild;
+	}
+}
 
+public ArrayList<node> getChildren(){
+	ArrayList<javax.xml.soap.Node> childNodes = new ArrayList<>();
+	if(this.leftChild != null)
+	{
+		childNodes.add(leftChild);
+	}
+	
 }
